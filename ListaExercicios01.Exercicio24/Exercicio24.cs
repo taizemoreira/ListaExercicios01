2075b0531512344d886ffb1ec3b410dc2c7b6547
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-03-13 06:09:29 -0300

    Alteração 1.1

--- ListaExercicios01.Exercicio24/Exercicio24.cs
+++ ListaExercicios01.Exercicio24/Exercicio24.cs
@@ -10,5 +10,19 @@ namespace ListaExercicios01.Exercicio24
 {
     internal class Exercicio24
     {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 500; i++)
+            {
+                if (i % 2 != 0 && i % 3 == 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine("Soma de todos os números ímpares: " + soma);
+        }
     }
 }
