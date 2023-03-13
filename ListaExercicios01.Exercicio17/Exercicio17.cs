9bbfbefce1cac3c14d65e7d35a5e0bcb7c85821d
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-03-13 00:54:48 -0300

    Alteração 1.1

---
+++ ListaExercicios01.Exercicio17/Exercicio17.cs
@@ -0,0 +1,29 @@
+﻿//17. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+namespace ListaExercicios01.Exercicio17
+{
+    public class Exercicio17
+    {
+        public static void Main(string[] args)
+        {
+            //Entrada de dados do usuario
+            Console.WriteLine("Insira o valor A: ");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor B: ");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor C: ");
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            //if verificador
+            if (A + B < C)
+            {
+                Console.WriteLine("A soma de A + B é menor que C ");
+            }
+            else
+            {
+                Console.WriteLine("A soma de A + B é maior ou igual a C");
+            }
+        }
+    }
+}
---
+++ ListaExercicios01.Exercicio18/Exercicio18.cs
@@ -0,0 +1,7 @@
+﻿//18.Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+namespace ListaExercicios01.Exercicio18
+{
+    internal class Exercicio18
+    {
+    }
+}
