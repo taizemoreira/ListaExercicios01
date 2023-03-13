1c1a7d941d1315520926d3b8d3eef4f5c02be3e5
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-03-13 01:01:46 -0300

    REfazer este exercício

--- ListaExercicios01.Exercicio18/Exercicio18.cs
+++ ListaExercicios01.Exercicio18/Exercicio18.cs
@@ -1,7 +1,19 @@
 ﻿//18.Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
 namespace ListaExercicios01.Exercicio18
 {
-    internal class Exercicio18
+    public class Exercicio18
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o primeiro valor: ");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo valor valor: ");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o terceiro valor: ");
+            int valor3 = Convert.ToInt32(Console.ReadLine());         
+        }
     }
 }
+
