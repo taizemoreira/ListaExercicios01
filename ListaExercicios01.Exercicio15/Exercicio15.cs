17aee2fb7c0c904a48b93276461290c4d7c18293
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-03-13 00:38:08 -0300

    Alteração 1.0

--- ListaExercicios01.Exercicio15/Exercicio15.cs
+++ ListaExercicios01.Exercicio15/Exercicio15.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//15. Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit. Faça um algoritmo para ler
+//uma temperatura Celsius e imprima em Fahrenheit (pesquise como fazer este tipo de conversão).
 
 namespace ListaExercicios01.Exercicio15
 {
-    internal class Exercicio15
+    public class Exercicio15
     {
+        public static void Main(string[] args)
+        {
+            //Entrada de dados --> informar temperatura em celcius
+            Console.Write("Informe a temperatura em Celsius: ");
+            double temperaturaCelcius = Convert.ToDouble(Console.ReadLine());
+
+            // Calculo --> conversão Fahrenheit
+            double temperaturaFahrenheit = (temperaturaCelcius * 1.8) + 32;
+
+            // Apresentar resultados na tela:
+            Console.WriteLine("A temperatura em Fahrenheit é: " + temperaturaFahrenheit.ToString("F1"));
+        }
     }
 }
--- ListaExercicios01.Exercicio16/Exercicio16.cs
+++ ListaExercicios01.Exercicio16/Exercicio16.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//16.Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%.
+//Após o aumento, desconte 8% de impostos. Imprima o salário inicial, o salário com o aumento e o salário final.
 
 namespace ListaExercicios01.Exercicio16
 {
     internal class Exercicio16
     {
+        public static void Main(string[] args)
+        {
+            //solicitação da entrada de dados
+            Console.WriteLine("Informe o salário do funcionário: ");
+            double salarioInicial = double.Parse(Console.ReadLine());
+
+            //calculo com aumento de salário do funcionario
+            double SalarioComAumento = salarioInicial * 1.15;
+
+            //descontando 8% de impostos
+            double salarioFinal = SalarioComAumento * 0.92;
+
+            //exibindo resultados na tela
+            Console.WriteLine("Salário inicial: R$ " + salarioInicial.ToString("F2"));
+            Console.WriteLine("Salário com o aumento: R$ " + SalarioComAumento.ToString("F2"));
+            Console.WriteLine("Salário final após descontar impostos: R$ " + salarioFinal.ToString("F2"));
+
+        }
     }
 }
