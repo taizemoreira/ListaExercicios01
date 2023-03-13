4986e89d0a06e7a01f63398baccba45822e5fc3e
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-03-12 23:48:12 -0300

    Alteração 1.0

---
+++ ListaExercicios01.Exercicio10/Exercicio10.cs
@@ -0,0 +1,41 @@
+﻿//10. Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicios01.Exercicio10
+{
+    internal class Exercicio10
+    {
+        static void Main(string[] args)
+        {
+            // Definição das variáveis
+            double prova1, prova2;
+            double peso1, peso2;
+            double mediaPonderada;
+
+            // Entrada de dados
+            Console.WriteLine("Digite a nota da primeira prova: ");
+            prova1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o peso da primeira prova: ");
+            peso1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da segunda prova: ");
+            prova2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o peso da segunda prova: ");
+            peso2 = double.Parse(Console.ReadLine());
+
+            // Calcular a média ponderada
+            mediaPonderada = ((prova1 * peso1) + (prova2 * peso2) / (peso1 * peso2));
+
+            // Exibir o resultado para o usuário
+            Console.WriteLine("A média ponderada das duas provas é: " + mediaPonderada);
+
+        }
+        
+    }
+}
---
+++ ListaExercicios01.Exercicio11/Exercicio11.cs
@@ -0,0 +1,36 @@
+﻿//11. Crie um programa para verificar se um número é primo.
+namespace ListaExercicios01.Exercicio11
+{
+    internal class Exercicio11
+    {
+        static void Main(string[] args)
+        {       
+            int numero;
+            int numeroInteiro;
+            int i = 0;
+
+            Console.WriteLine("Digite um numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            bool primo = true;
+
+            //comando for para verificação
+            for (numeroInteiro = 2; i < numero; i++)
+            {
+                if (numero % 1 == 0)
+                    {
+                        primo = false;
+                        break;
+                    }
+                }
+            if (primo == true && numero > 1)
+            {
+                Console.WriteLine(numero + " é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine(numero + " não é um número primo.");
+            }
+   
+        }
+    }
+}
